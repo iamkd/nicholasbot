320989c0b6c2bfd8579615f1b05d7e412c1e83cc
Danyil Karuna <kd@oked.me>
2017-12-17 22:24:08 +0200

    Pairs count list

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,11 @@ const app = express();
 app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'pug');
 
+app.get('/pairs', async (req, res) => {
+  const users = await User.find({}).exec();
+  res.render('pairs', { users: R.sort((a, b) => a.count - b.count, users) });
+});
+
 app.get('*', async (req, res) => {
   const users = await User.find({}).exec();
   res.render('index', { users: R.sort((a, b) => a.count - b.count, users) });
